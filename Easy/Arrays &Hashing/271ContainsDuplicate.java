34bbf00f0b6e551bb08e6e8bd480f257fa18d3b9
Digvijay <93901013+digvijayizing@users.noreply.github.com>
2023-12-17 12:48:53 +0530

    Update 271ContainsDuplicate.java

--- Easy/Arrays &Hashing/271ContainsDuplicate.java
+++ Easy/Arrays &Hashing/271ContainsDuplicate.java
@@ -9,3 +9,12 @@ class Solution {
        }
     return false;}
 }
+
+// TC - O(n)
+// SC -O(n)  the code creates a HashSet that can potentially store all the elements of the array.
+
+/* Hashset is used because only unique elements can be added to the hashset , (here for first iteration there is no element in the array so either way it is going to be added into the hashset)
+
+then for the next iteration it checks that if the hashset contains the integer that is the num already exist in the hashset or not ,if it does just return true or else add it to the set 
+
+*/
