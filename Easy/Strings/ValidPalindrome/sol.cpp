0810c4c9397ab478fc7ebcce1bb056a21659564b
Digvijay <93901013+digvijayizing@users.noreply.github.com>
2024-01-17 13:40:43 +0530

    Update sol.cpp

--- Easy/Strings/ValidPalindrome/sol.cpp
+++ Easy/Strings/ValidPalindrome/sol.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string x;
+        for(int i =s.size()-1;i>=0;i--){
+            // if(isspace(s[i]))continue;
+            if( isalnum(s[i]) ) { // LEARNED ABOUT ISALNUM AND TOLOWER 
+                x+=tolower(s[i]);
+                }
+        }
+        
+        int low=0,high=x.size()-1;
+       
+        while(low<=high){
+            if(x[low]!=x[high])return false;
+            low++;
+            high--;
+        }
+    return true;}
+};
