268429ae889ae8a2d5c0030ee195e514636e6fd9
Digvijay <93901013+digvijayizing@users.noreply.github.com>
2024-01-17 14:12:44 +0530

    Update sol.cpp

--- Easy/Strings/ValidPalindrome/sol.cpp
+++ Easy/Strings/ValidPalindrome/sol.cpp
@@ -1,3 +1,4 @@
+METHOD 1 - WITH INBUILT FUNCTION
 class Solution {
 public:
     bool isPalindrome(string s) {
@@ -15,6 +16,44 @@ public:
             if(x[low]!=x[high])return false;
             low++;
             high--;
+        }
+    return true;}
+};
+
+
+
+METHOD 2 - BRUTE 
+
+    
+class Solution {
+    private:
+    bool valid(char ch){
+if( (ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9'))
+   { return 1; }
+    return 0;}
+
+    char lower(char c){
+        if( (c>='a' && c<='z') || (c>='0' && c<='9') ){
+            return c;
+        }
+        return (c-'A'+'a');
+    }
+public:
+    bool isPalindrome(string s) {
+        string x;
+        for(auto i :s){
+            if(valid(i)) x+=i;
+        }
+       
+        for(int i=0; i<x.size();i++){
+            x[i]= lower(x[i]);
+        }
+        int low=0,high=x.size()-1;
+        while(low<high){
+            if(x[low]!=x[high])return false;
+            low++;
+            high--;
+        
         }
     return true;}
 };
